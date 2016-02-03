44d779a49bcdadbb341a852112d60b502f81e310
Schuyler Reinken <sreinken3@gatech.edu>
2016-02-02 19:01:58 -0500

    Login Screen Packages made

--- app/src/main/java/com/example/schuylerreinken/moviematcher/controllers/MainActivity.java
+++ app/src/main/java/com/example/schuylerreinken/moviematcher/controllers/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.schuylerreinken.moviematcher;
+package com.example.schuylerreinken.moviematcher.controllers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.schuylerreinken.moviematcher.R;
 
 public class MainActivity extends AppCompatActivity {
 
