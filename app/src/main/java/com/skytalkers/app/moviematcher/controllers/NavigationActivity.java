31d52977ccfbea99e401570d878fa7eb2855a0a2
Schuyler Reinken <sreinken3@gatech.edu>
2016-02-18 13:45:06 -0500

    --

--- app/src/main/java/com/skytalkers/app/moviematcher/controllers/NavigationActivity.java
+++ app/src/main/java/com/skytalkers/app/moviematcher/controllers/NavigationActivity.java
@@ -88,8 +88,8 @@ public class NavigationActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
             myFragment = new SearchFragment();
-        } else if (id == R.id.nav_gallery) {
-
+        } else if (id == R.id.nav_search) {
+            myFragment = new SearchFragment();
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
