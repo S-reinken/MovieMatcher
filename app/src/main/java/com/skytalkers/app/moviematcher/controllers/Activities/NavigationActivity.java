805c23b57477466a789b02b25dfe926184099cd8
Schuyler Reinken <sreinken3@gatech.edu>
2016-02-23 18:05:30 -0500

    Floating Action Button removed from NavigationActivity

--- app/src/main/java/com/skytalkers/app/moviematcher/controllers/Activities/NavigationActivity.java
+++ app/src/main/java/com/skytalkers/app/moviematcher/controllers/Activities/NavigationActivity.java
@@ -43,6 +43,7 @@ public class NavigationActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        /*
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +51,7 @@ public class NavigationActivity extends AppCompatActivity
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
